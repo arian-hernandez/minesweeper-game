e919c9e7d3d0b8b5d91ebfb7e89d0fa40bd083fa
arian-hernandez <arian@ideagomedia.com>
2025-01-03 12:04:07 -0500

    Added buttons to reset game and return to game tittle

--- src/main.js
+++ src/main.js
@@ -2,26 +2,28 @@ import { Cell, SafeCell } from "./cell.js";
 import Board from "./board.js";
 import Game from "./game.js";
 
-// Esperar a que el DOM esté completamente cargado
-document.addEventListener("DOMContentLoaded", () => {
+
+
   // Obtener los parámetros de la URL
   const params = new URLSearchParams(window.location.search);
   const difficulty = params.get("difficulty");
 
-  if (difficulty) {
+ 
     const settings = { difficulty };
-    console.log("Starting game with settings:", settings);
 
     // Inicializar el juego
-    const game = new Game(settings);
-    const board = new Board(game);
+    let game = new Game(settings);
+    let board = new Board(game);
     
 
     // Aquí puedes inicializar o renderizar el tablero
     board.initializeBoard();
-  } else {
-    console.error("No difficulty selected!");
-    // Opcional: Redirigir de vuelta al menú principal
-    window.location.href = "index.html";
-  }
+  
+
+
+const ElementResetButton = document.querySelector('.js-reset-button');
+ElementResetButton.addEventListener('click',()=>{
+  game = new Game(settings);
+  board = new Board(game);
+  board.initializeBoard();  
 });
